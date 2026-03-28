a0fbd481662f0effb6abb898e7b8f1531319bb3f
matetremmel <matetremmel3@gmail.com>
2026-03-28 13:08:46 +0000

    Initial project setup

---
+++ main.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import sklearn
+
+print("Environment is working")
+
+from src.config import PROJECT_ROOT, DATA_DIR, MODELS_DIR
+
+print("Project root:", PROJECT_ROOT)
+print("Data dir:", DATA_DIR)
+print("Models dir:", MODELS_DIR)
---
+++ src/config.py
@@ -0,0 +1,7 @@
+from pathlib import Path
+
+PROJECT_ROOT = Path(__file__).resolve().parent.parent
+DATA_DIR = PROJECT_ROOT / "data"
+NOTEBOOKS_DIR = PROJECT_ROOT / "notebooks"
+MODELS_DIR = PROJECT_ROOT / "models"
+REPORTS_DIR = PROJECT_ROOT / "reports"
